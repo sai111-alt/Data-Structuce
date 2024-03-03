043aff9f29c2039f406c494eefe31df2177ab8d3
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2024-03-03 22:53:10 +0800

    Study

--- 1_Time and space complexity/1_Time and space complexity/1.c
+++ 1_Time and space complexity/1_Time and space complexity/1.c
@@ -1,8 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+#include<stdio.h>
+
+void Func1(int N)
+{
+	int count = 0;
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			++count;
+		}
+	}
+	for (int k = 0; k < 2 * N; ++k)
+	{
+		++count;
+	}
+	int M = 10;
+	while (M--)
+	{
+		++count;
+	}
+	printf("%d\n", count);
+}
+
 int main()
 {
-
-
-	return 0;
+	Func1(2);
 }
