1f36de5f66a720fe82d843a317b86a97d30bc181
sai111-alt <78285309+sai111-alt@users.noreply.github.com>
2024-03-08 22:56:06 +0800

    Study

--- 1_Time and space complexity/1_Time and space complexity/1.c
+++ 1_Time and space complexity/1_Time and space complexity/1.c
@@ -54,6 +54,16 @@ void Func3(int N, int M)
 	printf("%d\n", count);
 }
 
+void Fun4(int N)
+{
+	int count = 0;
+	for (int k = 0; k < 100; ++k)
+	{
+		++count;
+	}
+	printf("%d\n", count);
+}
+
 int main()
 {
 	Func1(2);
